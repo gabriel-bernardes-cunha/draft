4a74421054b7a489e3a91cb6264c6ab144ad0987
gabriel-bernardes-cunha <gabriel.bernardes@unifesp.br>
2026-05-10 00:02:51 -0300

    Update beecrowd 1406.c

--- beecrowd 1406.c
+++ beecrowd 1406.c
@@ -9,14 +9,44 @@ int main() {
     
     for (int i=0;i<N;i=++) {
         //%30 para permitir ler no máximo 30 caracteres!
-        scanf("%30s",nome[i])
-        scanf("%30s",artista[i])
-
+        //Primeiramente, vamos atribuir as múscias e os artistas às matrizes.
+        scanf("%30s",nome[i]);
+        scanf("%30s",artista[i]);
+        
         for (int p=0;p;p++) {
         //quando nome[i][j]='\0' break, até lá, 
-
+       
         }
     }
- 
+    //Agora, a tal "string de ouro" para cada termo, isto é, cada linha das duas matrizes anteriores.
+    //Vamos criar uma matriz com as strings de ouro.
+    char oro [N][31];
+    for (int i=0;i<N;i=++) {
+       //O próximo laço scaneia uma linha z, para cada outra linha, vendo suas diferenças.
+       for (int z=0;z<N;z++) {
+          if (z!=i) {
+             int u=0;
+             //Agora, vemos cada caractere em uma linha.
+             for (int x=0;x<30;x++) {
+                 if (nome[i][x]!=nome[z][x]) {
+                      oro[i][u]=nome[i][x];
+      
+                 }
+             }
+          }
+          /*else {
+
+
+          }*/
+       }
+       /*for (int t=0;t<30;t++) {
+        
+          if (nome[i][t]=) {
+  
+             oro[i][u]=nome[i][t];
+           
+          }
+       }*/
+    
     return 0;
 }
