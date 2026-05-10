47fa7773f9ef777ad44927352f6769ceec00d582
gabriel-bernardes-cunha <gabriel.bernardes@unifesp.br>
2026-05-09 21:38:29 -0300

    Create beecrowd 1406.c

---
+++ beecrowd 1406.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+ 
+int main() {
+
+    int N;
+    scanf("%d",&N);
+    char nome [N] [31];
+    char artista [N] [31];
+    
+    for (int i=0;i<N;i=++) {
+        //%30 para permitir ler no máximo 30 caracteres!
+        scanf("%30s",nome[i])
+        scanf("%30s",artista[i])
+
+        for (int p=0;p;p++) {
+        //quando nome[i][j]='\0' break, até lá, 
+
+        }
+    }
+ 
+    return 0;
+}
